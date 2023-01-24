8354264f579efedda43cdce3fb10a2a49d47a5eb
Conor Lewis <55404148+LewisConor@users.noreply.github.com>
2023-01-24 16:43:31 +0000

    Basic Level Generator - Start
    Plans to expand on this into more branchy levels depending what we discuss on this week.

---
+++ Assets/Scripts/Conor/LevelGeneratorEditor.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(LevelGenerator))]
+public class LevelGeneratorEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        DrawDefaultInspector();
+
+        LevelGenerator levelGen = (LevelGenerator)target;
+
+        if(Application.isPlaying)
+        {
+            if(GUILayout.Button("Generate Seed")) { levelGen.GenerateSeed(); }
+            if(GUILayout.Button("Generate Level")) { levelGen.GenerateLevel(); }
+            if(GUILayout.Button("Clear Level")) { levelGen.ClearLevel(); }
+        }
+    }
+}
