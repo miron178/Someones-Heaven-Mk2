656f7599da95565ebd366dee402c9a83ba628027
Miron Bury <miron.bury@gmail.com>
2023-01-29 20:53:05 +0000

    Light mechanic base

--- Assets/Miron/Scripts/LightSubtractionScript.cs
+++ Assets/Miron/Scripts/LightSubtractionScript.cs
@@ -4,15 +4,42 @@ using UnityEngine;
 
 public class LightSubtractionScript : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField]
+    Material[] materials;
 
-    // Update is called once per frame
-    void Update()
+    enum MaterialType
     {
-        
+        LightSolid = 0,
+        LightTrans,
+        ShadowSolid,
+        ShadowTrans,
+    };
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Light")
+        {
+            other.gameObject.GetComponent<Collider>().isTrigger = false;
+            other.gameObject.GetComponent<MeshRenderer>().material = materials[(int)MaterialType.LightSolid];
+        }
+        else if (other.tag == "Shadow")
+        {
+            other.gameObject.GetComponent<Collider>().isTrigger = true;
+            other.gameObject.GetComponent<MeshRenderer>().material = materials[(int)MaterialType.ShadowTrans];
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        Material otherMaterial = other.gameObject.GetComponent<Material>();
+        if (other.tag == "Light")
+        {
+            other.gameObject.GetComponent<Collider>().isTrigger = true;
+            other.gameObject.GetComponent<MeshRenderer>().material = materials[(int)MaterialType.LightTrans];
+        }
+        else if (other.tag == "Shadow")
+        {
+            other.gameObject.GetComponent<Collider>().isTrigger = false;
+            other.gameObject.GetComponent<MeshRenderer>().material = materials[(int)MaterialType.ShadowSolid];
+        }
     }
 }
