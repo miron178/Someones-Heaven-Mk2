1eea9c4db63d0f264d6957fe2864076caf31c3f7
BerrenStone <57990042+BerrenStone@users.noreply.github.com>
2023-03-09 14:34:45 +0000

    Push

--- Assets/Bez/Script/Traps.cs
+++ Assets/Bez/Script/Traps.cs
@@ -8,17 +8,6 @@ public class Traps : MonoBehaviour
 	
 	
 	
-	// Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 
 	public void OnTriggerEnter(Collider other) {
 		player.TakeDamage(1);
