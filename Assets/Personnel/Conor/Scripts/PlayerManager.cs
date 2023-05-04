abdfba2fc2d72d43fd73cf398a09f3df0ced4206
Conor Lewis <55404148+LewisConor@users.noreply.github.com>
2023-05-04 12:34:06 +0100

    Game Manager Beta

---
+++ Assets/Personnel/Conor/Scripts/PlayerManager.cs
@@ -0,0 +1,19 @@
+using System;
+using UnityEngine;
+
+public class PlayerManager : MonoBehaviour
+{
+    private static PlayerManager m_instance;
+    public static PlayerManager Instance { get { return m_instance; } }
+
+    [SerializeField] GameObject m_playerPrefab;
+    GameObject m_player;
+    public void SpawnPlayer() { m_player = Instantiate(m_playerPrefab, new Vector3(0,0,0), Quaternion.identity); }
+    public void DeletePlayer() { Destroy(m_player); m_player = null; }
+
+    void Awake()
+    {
+        if(!m_instance) { m_instance = this; }
+        else { Destroy(this); }
+    }
+}
