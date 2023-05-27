57e38532d5df63e3c612a58f619764a0238207c0
Miron Bury <miron.bury@gmail.com>
2023-05-27 22:16:29 +0100

    fix look dir while aiming
    now works with controller

--- Assets/Personnel/Miron/Scripts/FollowMouse.cs
+++ Assets/Personnel/Miron/Scripts/FollowMouse.cs
@@ -7,20 +7,36 @@ public class FollowMouse : MonoBehaviour
 {
     [SerializeField]
     Camera useCamera;
+
+    Vector3 direction = Vector3.forward;
+
+    Vector3 oldMousePos = Vector3.zero;
+
     void Update()
     {
         LookAtMouse();
+        transform.rotation = Quaternion.LookRotation(direction);
     }
 
     void LookAtMouse()
     {
-        //redo for controler (second function?)
         Vector3 mousePos = Mouse.current.position.ReadValue();
-        Ray ray = useCamera.ScreenPointToRay(mousePos);
-        Plane plane = new Plane(Vector3.up, transform.position);
-        plane.Raycast(ray, out float distance);
-        Vector3 target = ray.GetPoint(distance);
+        if (oldMousePos != mousePos)
+        {
+            oldMousePos = mousePos;
 
-        transform.LookAt(target);
+            Ray ray = useCamera.ScreenPointToRay(mousePos);
+            Plane plane = new Plane(Vector3.up, transform.position);
+            plane.Raycast(ray, out float distance);
+            Vector3 target = ray.GetPoint(distance);
+
+            direction = target - transform.position;
+        }
+    }
+
+    public void OnLook(InputAction.CallbackContext context)
+    {
+        Vector2 pad = context.ReadValue<Vector2>();
+        direction = new Vector3(pad.x, 0, pad.y);
     }
 }
